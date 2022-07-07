aa3605e0e2a9f24d9e46a8803283c26e4be4f15f
snowyuki31 <snowyuki31@users.noreply.github.com>
2022-07-07 16:42:48 +0000

    deploy: d9bbc3601fff0e36aacaa6d0cf01d07e362defa1

---
+++ _next/static/chunks/pages/linkSample-a64c8feaea886ad6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[82],{5451:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/linkSample",function(){return t(9689)}])},9689:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return _}});t(5893);function _(){}}},function(n){n.O(0,[774,888,179],(function(){return u=5451,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
