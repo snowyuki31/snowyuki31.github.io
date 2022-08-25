23bd9a506995ff6c15ecf97b23b3c4d223ea3f71
snowyuki31 <snowyuki31@users.noreply.github.com>
2022-08-25 14:43:04 +0000

    deploy: 76bd2a5363a6e640805689949588643cdffbe43b

---
+++ _next/static/QaAz2zJTJe1QQpNRM1ASe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/721-3628e6c59a6e722a.js","static/css/8034fd035ae25f0f.css","static/chunks/pages/index-a34359d57d15e7c5.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
